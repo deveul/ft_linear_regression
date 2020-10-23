8360d6c10f4202e62269472a0015f503f87ded13
VladimirRenaudin <vladimir.renaudin@hotmail.fr>
2020-10-23 19:40:31 +0200

    graph with straight line

--- graph.py
+++ graph.py
@@ -2,19 +2,29 @@
 # -*-coding:Utf-8 -*
 
 import matplotlib.pyplot as plt
+import numpy as np
 
-def graph(data):
-    plt.axes = ([0, max(data, key=lambda x:x['km']), 0, max(data, key=lambda x:x['km'])])
+def graph(data, theta_0, theta_1):
+    plt.axes = ([min(data, key=lambda x:x['km'])['km'], max(data, key=lambda x:x['km'])['km'], \
+        min(data, key=lambda x:x['price'])['price'], max(data, key=lambda x:x['price'])['price']])
     # axes = plt.gca()
     # axes.set_xlim([-2.0, 2.0])
     # axes.set_ylim([min(small_results) - 0.5, max(small_results) + 0.5])
     plt.axhline(0, color='grey')
     plt.axvline(0, color='grey')
-    for row in data:
-        plt.scatter(row['km'], row['price'], c='purple')
-    # plt.legend(loc="upper left")
     plt.xlabel('km')
     plt.ylabel('price')
     plt.title('Linear Regression')
     
+    # 100 linearly spaced numbers
+    x = np.linspace(min(data, key=lambda x:x['km'])['km'], max(data, key=lambda x:x['km'])['km'], 100)
+    y = theta_0 + theta_1 * x
+    
+    # On affiche les points du data set
+    for row in data:
+        plt.scatter(row['km'], row['price'], c='purple')
+    # On affiche la droite avec les valeurs de theta trouvées
+    plt.plot(x, y, linestyle='-', label="price = {} {} * km".format(round(theta_0, 2), round(theta_1, 2)))
+    
+    # plt.legend(loc="lower left")
     plt.show()
